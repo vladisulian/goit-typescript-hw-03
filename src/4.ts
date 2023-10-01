38105a43528a5deafb4743e622179e8e86d4b16b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-01 23:49:06 +0300

    add interface IKey and class Key

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,28 @@
+interface IKey {
+  readonly key: number;
+
+  getSignature(): number;
+}
+
+class Key implements IKey {
+   key: number = Math.random();
+
+  getSignature(): number {
+    return this.key;
+  }
+}
+
+class House {}
+class MyHouse {}
+class Person {}
+
 const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// const house = new MyHouse(key);
+// const person = new Person(key);
 
-house.openDoor(person.getKey());
+// house.openDoor(person.getKey());
 
-house.comeIn(person);
-
+// house.comeIn(person);
 
 export {};
