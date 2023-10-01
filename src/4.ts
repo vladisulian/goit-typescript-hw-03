25101acf02bc37ecfbc87d14c56f5143c84f7946
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-02 00:12:23 +0300

    upd. 4.ts, add Person and IPerson

--- src/4.ts
+++ src/4.ts
@@ -4,22 +4,41 @@ interface IKey {
   getSignature(): number;
 }
 
+interface IPerson {
+  readonly key: object;
+
+  getKey(): number;
+}
+
 class Key implements IKey {
-   key: number = Math.random();
+  key: number = Math.random();
 
   getSignature(): number {
     return this.key;
   }
 }
 
+class Person implements IPerson {
+  key: object;
+
+  constructor(key: object) {
+    this.key = key;
+  }
+
+  getKey(): number {
+    console.log(this.key);
+    return key.key;
+  }
+}
+
 class House {}
 class MyHouse {}
-class Person {}
 
 const key = new Key();
 
 // const house = new MyHouse(key);
-// const person = new Person(key);
+const person = new Person(key);
+person.getKey();
 
 // house.openDoor(person.getKey());
 
